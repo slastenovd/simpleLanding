d44e366e4535825777e92563931905869a42f4e3
slastenovd <slastenovd@mail.ru>
2016-11-14 22:44:21 +1000

    first

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+
+	$( ".dimka" ).on( "click", function() {
+	  $(".ad").fadeOut();
+
+	  var id = 12345676;
+	  id = id / 5;
+
+	  console.log(id);
+	});	
+
+
+
+
+
+});
